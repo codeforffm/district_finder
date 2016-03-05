c2864e587066f718a6d2c917705ce768b68c1ab3
Headbird Studio <shinsen_industries@outlook.com>
2016-03-05 21:04:19 +0100

    districCenters korrigiert

--- scripts/Test.ts
+++ scripts/Test.ts
@@ -4,7 +4,7 @@
 
 function GetPrices() {
     var prices = DistrictFinder.SerializationHelper.toInstance(new DistrictFinder.Rentprices(), "rentPrices.json");
-    //var positions = DistrictFinder.SerializationHelper.toInstance(new DistrictFinder.DistrictCenters(), "districtCenters.json");
+    var positions = DistrictFinder.SerializationHelper.toInstance(new DistrictFinder.DistrictCenters(), "districtCenters.json");
 
     var data : Array<number>[];  
     data = new Array();
@@ -17,8 +17,16 @@ function GetPrices() {
     });
         
     for (var i = 0; i < prices.prices.length; i++) {
-        var item = [8.669173, 50.106046, parseFloat(parseFloat(prices.prices[i].prices) / parseFloat(highest))];
-        data.push(item);
+
+        for(var y = 0; y < positions.districts.length; y++)
+        {
+            if(positions.districts[y].district == prices.prices[i].district)
+            {
+                var item = [parseFloat(positions.districts[y].x), parseFloat(positions.districts[y].y), parseFloat(parseFloat(prices.prices[i].prices) / parseFloat(highest))];
+                data.push(item);
+                break;
+            }            
+        }
     }
     
     return data;
--- scripts/js/main.js
+++ scripts/js/main.js
@@ -57,7 +57,7 @@ var DistrictFinder;
 /// <reference path="SerializationHelper"/>
 function GetPrices() {
     var prices = DistrictFinder.SerializationHelper.toInstance(new DistrictFinder.Rentprices(), "rentPrices.json");
-    //var positions = DistrictFinder.SerializationHelper.toInstance(new DistrictFinder.DistrictCenters(), "districtCenters.json");
+    var positions = DistrictFinder.SerializationHelper.toInstance(new DistrictFinder.DistrictCenters(), "districtCenters.json");
     var data;
     data = new Array();
     var highest;
@@ -67,8 +67,13 @@ function GetPrices() {
             highest = element.prices;
     });
     for (var i = 0; i < prices.prices.length; i++) {
-        var item = [8.669173, 50.106046, parseFloat(parseFloat(prices.prices[i].prices) / parseFloat(highest))];
-        data.push(item);
+        for (var y = 0; y < positions.districts.length; y++) {
+            if (positions.districts[y].district == prices.prices[i].district) {
+                var item = [parseFloat(positions.districts[y].x), parseFloat(positions.districts[y].y), parseFloat(parseFloat(prices.prices[i].prices) / parseFloat(highest))];
+                data.push(item);
+                break;
+            }
+        }
     }
     return data;
 }
