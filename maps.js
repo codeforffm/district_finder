3a874ec3e78ef504c23d851d190a4a5761ba39f4
egde <degertsa@yahoo.com>
2016-03-05 11:41:50 +0100

    Refactored the code and added samples on how to use it in an HTML

---
+++ maps.js
@@ -0,0 +1,55 @@
+function buildMap(target, coord, layers) {
+  var map = new ol.Map({
+    target: target,
+    layers: layers,
+    view: new ol.View({
+      center: ol.proj.fromLonLat(coord),
+      zoom: 10
+    })
+  });
+  return map;
+}
+
+function osmMapLayer() {
+  var layer = new ol.layer.Tile({
+    source: new ol.source.OSM()
+  });
+  return layer;
+}
+
+function districtLayer() {
+  var districtsLayer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+    url: 'https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/frankfurt-main.geojson',
+    format: new ol.format.GeoJSON({
+      defaultDataProjection :'EPSG:4326', 
+      projection: 'EPSG:3857'
+    })
+   })
+  });
+  return districtsLayer;
+}
+
+function heatmapLayer(values) {
+  var data = new ol.source.Vector();
+
+  for (var i = 0; i< values.length; i++) {
+    var v = values[i];
+    var coord = ol.proj.transform([v[0], v[1]],'EPSG:4326', 'EPSG:3857');
+    var lonLat = new ol.geom.Point(coord);
+    var pointFeature = new ol.Feature({
+      geometry: lonLat,
+      weight:v[2]
+    });
+
+    data.addFeature(pointFeature);
+  }
+  
+  //building the heatmap layer
+  var heatmapLayer = new ol.layer.Heatmap({
+    source:data,
+    radius:30
+  });
+
+  return heatmapLayer;
+}
